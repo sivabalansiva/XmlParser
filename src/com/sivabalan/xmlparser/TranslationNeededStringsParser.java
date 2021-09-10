d8440f88c5ac0d374e09c74e140f5b466c60c8c5
sivabalan.t <sivabalan.t@zohocorp.com>
2021-09-10 17:00:20 +0530

    Added support to remove non translatable strings when displaying output

--- src/com/sivabalan/xmlparser/TranslationNeededStringsParser.java
+++ src/com/sivabalan/xmlparser/TranslationNeededStringsParser.java
@@ -4,22 +4,24 @@ import javax.xml.namespace.QName;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Attribute;
 import javax.xml.stream.events.XMLEvent;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class TranslationNeededStringsParser {
 
     private static final String STRING = "string";
     private static final String NAME = "name";
+    private static final String TRANSLATABLE = "translatable";
+
+    private final boolean printLog = false;
 
     private final XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
     private final ArrayList<File> stringFilesList = new ArrayList<>();
     private final HashMap<String, String> strings = new HashMap<>();
-    private final HashMap<File, HashMap<String,String>> table = new HashMap<>();
+    private final HashMap<File, HashMap<String, String>> table = new HashMap<>();
+    private final HashSet<String> nonTranslatableStringsSet = new HashSet<>();
 
     static TranslationNeededStringsParser newInstance() {
         return new TranslationNeededStringsParser();
@@ -31,7 +33,7 @@ public class TranslationNeededStringsParser {
         obj.init(url);
     }
 
-    private String getUrl()   {
+    private String getUrl() {
         System.out.print("Enter the url...");
         Scanner scan = new Scanner(System.in);
         return scan.next();
@@ -44,26 +46,26 @@ public class TranslationNeededStringsParser {
         printOutput();
     }
 
-    private void fetchAllStringFiles(String url)    {
+    private void fetchAllStringFiles(String url) {
         File dir = new File(url);
         File[] files = dir.listFiles();
-        if(files == null) {
+        if (files == null) {
             System.out.println("No such url (or) no files found.");
             return;
         }
-        for(File file : files)  {
-            if(file.isFile())   {
+        for (File file : files) {
+            if (file.isFile()) {
                 String fileName = file.getName();
-                if(fileName.equals("strings.xml"))    {
+                if (fileName.equals("strings.xml")) {
                     stringFilesList.add(file);
                 }
-            } else  {
+            } else {
                 fetchAllStringFiles(file.toString());
             }
         }
     }
 
-    private void parseAllStringFiles()  {
+    private void parseAllStringFiles() {
         for (File file : stringFilesList) {
             try {
                 parse(file);
@@ -78,57 +80,76 @@ public class TranslationNeededStringsParser {
             InputStream stream = new FileInputStream(file);
             XMLEventReader reader = xmlInputFactory.createXMLEventReader(stream);
             HashMap<String, String> stringsInFile = new HashMap<>();
-            while(reader.hasNext()) {
+            while (reader.hasNext()) {
                 XMLEvent event = reader.nextEvent();
-                if(event.isStartElement() && event.asStartElement().getName().getLocalPart().equals(STRING))    {
+                if (event.isStartElement() && event.asStartElement().getName().getLocalPart().equals(STRING)) {
                     String key = event.asStartElement().getAttributeByName(new QName(NAME)).getValue();
-                    System.out.println("File -> " + file.getAbsolutePath() + ", Key: " + key);
+                    Attribute isTranslatable = event.asStartElement().getAttributeByName(new QName(TRANSLATABLE));
+                    if (isTranslatable != null && "false".equalsIgnoreCase(isTranslatable.getValue())) {
+                        nonTranslatableStringsSet.add(key);
+                    }
+                    printLog("File -> " + file.getAbsolutePath() + ", Key: " + key);
                     String value = reader.getElementText();
                     stringsInFile.put(key, value);
-                    if(!strings.containsKey(key))    {
+                    if (!strings.containsKey(key)) {
                         strings.put(key, value);
                     }
-//                    System.out.println(key + " -> " + value);
+//                    printLog(key + " -> " + value);
                 }
             }
             table.put(file, stringsInFile);
         } catch (FileNotFoundException e) {
-            System.err.println(file.toString() + " file not found.");
+            System.err.println(file + " file not found.");
             e.printStackTrace();
         }
     }
 
-    private void printOutput()  {
+    private void printOutput() {
         StringBuilder output = new StringBuilder("Total strings: " + strings.size() + "\n");
-        for(Map.Entry<File, HashMap<String, String>> m : table.entrySet())  {
+        for (Map.Entry<File, HashMap<String, String>> m : table.entrySet()) {
             File file = m.getKey();
             HashMap<String, String> stringsInFile = m.getValue();
             output.append("String count in ")
                     .append(file.getParentFile().getName())
                     .append(": ")
                     .append(stringsInFile.size())
-                    .append("\t\t\t Required strings count: ")
-                    .append(strings.size() - stringsInFile.size())
-                    .append("\t\tRequired in ")
+                    .append("\t\t\t Required strings count: ");
+
+            int index = output.length();
+
+            output.append("\t\tRequired in ")
                     .append(file.getParentFile().getName())
                     .append("\n");
-            for(Map.Entry<String, String> ss : strings.entrySet())    {
-                if(!stringsInFile.containsKey(ss.getKey()))    {
+
+            int requiredStringsCount = 0;
+            for (Map.Entry<String, String> ss : strings.entrySet()) {
+                if (nonTranslatableStringsSet.contains(ss.getKey())) {
+                    continue;
+                }
+                if (!stringsInFile.containsKey(ss.getKey())) {
                     output.append("<string name=\"")
                             .append(ss.getKey())
-                            .append("\" >")
+                            .append("\">")
                             .append(ss.getValue())
                             .append("</string>\n");
+                    ++requiredStringsCount;
                 }
             }
+            output.insert(index, requiredStringsCount);
             output.append("\n\n");
         }
         File outputFile = new File("output.txt");
-        try(FileOutputStream fos = new FileOutputStream(outputFile)) {
+        try (FileOutputStream fos = new FileOutputStream(outputFile)) {
             fos.write(output.toString().getBytes());
             System.out.println("Output written to file..." + outputFile.getAbsolutePath());
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void printLog(String log) {
+        if (printLog) {
+            System.out.println(log);
         }
     }
 }
